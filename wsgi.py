7f1763f77d1c833607c881f952502db9f2fbbb8f
cftang <tangchunfeng@gmail.com>
2017-09-17 19:54:29 +0800

    map 3a

--- wsgi.py
+++ wsgi.py
@@ -6,15 +6,15 @@ def hello():
     return "Hello World! python 3.5"
 
 @application.route("/map1")
-def hello():
+def map1():
     return "Hello World! map1"
 
 @application.route("/map2")
-def hello():
+def map2():
     return "Hello World! map2"
 
 @application.route("/map3")
-def hello():
+def map3():
     return "Hello World! map3"
 
 if __name__ == "__main__":
