e9835912bbc8401b1fb1d603ae49a200dcc112de
cftang <tangchunfeng@gmail.com>
2017-09-17 19:42:17 +0800

    map 3

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,17 @@ application = Flask(__name__)
 def hello():
     return "Hello World! python 3.5"
 
+@application.route("/map1")
+def hello():
+    return "Hello World! map1"
+
+@application.route("/map2")
+def hello():
+    return "Hello World! map2"
+
+@application.route("/map3")
+def hello():
+    return "Hello World! map3"
+
 if __name__ == "__main__":
     application.run()
