ce1d14154351ea56c86d5e48cca3e45cbb6c7d9e
tangchunfeng@gmail.com <tangchunfeng@gmail.com>
2017-10-05 20:02:56 -0700

    heartbeat

---
+++ scripts/sd.heartbeat.py
@@ -0,0 +1,23 @@
+import pika
+import json
+import time
+
+parameters = pika.URLParameters('amqp://rtjojptm:wTajVRnB9IWJgI1KdHBdLLSxaAJtFTHm@mosquito.rmq.cloudamqp.com/rtjojptm')
+connection = pika.BlockingConnection(parameters)
+
+# Create normal 'Hello World' type channel.
+channel = connection.channel()
+channel.confirm_delivery()
+channel.queue_declare(queue='h1', durable=True)
+
+# We need to bind this channel to an exchange, that will be used to transfer
+# messages from our delay queue.
+channel.queue_bind(exchange='amq.direct',
+                   queue='h1')
+d = {'distance': 0}
+for num in range(1, 200000000):
+    channel.basic_publish(exchange='amq.direct',
+                          routing_key='h1',
+                          body=json.dumps(d),
+                          properties=pika.BasicProperties(delivery_mode=2))
+    time.sleep(60)
--- scripts/wget.sh
+++ scripts/wget.sh
@@ -5,6 +5,6 @@ do
 rm map*
 #wget http://127.0.0.1:8080/map2
 #sleep 300
-w                                                                                                                                                                                                                                                                                                                                               get http://127.0.0.1:8080/map3
+wget http://127.0.0.1:8080/map3
 sleep 300
 done
