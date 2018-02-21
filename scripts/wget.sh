7aacda1cede68e4fa7ae99e29e6a9d0f3f962d7a
cftang <tangchunfeng@gmail.com>
2018-02-21 12:06:10 +0800

    indent

--- scripts/wget.sh
+++ scripts/wget.sh
@@ -2,10 +2,10 @@
 cd /tmp
 while true 
 do
-rm map*
-#wget http://127.0.0.1:8080/map2
-#sleep 300
-wget http://127.0.0.1:8080/map3
-python ~/test1.py
-sleep 300
+    rm map*
+    #wget http://127.0.0.1:8080/map2
+    #sleep 300
+    wget http://127.0.0.1:8080/map3
+    python ~/test1.py
+    sleep 300
 done
