4b89dce87329b173000a64c9fd454bf82e7a328a
cftang <tangchunfeng@gmail.com>
2018-02-21 19:01:48 +0800

    generateChart added

--- scripts/nohup.sh
+++ scripts/nohup.sh
@@ -7,6 +7,8 @@ nohup python /opt/app-root/src/scripts/sd.heartbeat.py > /tmp/heartbeat.out &
 
 nohup sh /opt/app-root/src/scripts/wget.sh > /tmp/wget.out &
 
+nohup sh /opt/app-root/src/scripts/generateChart.sh > /tmp/generateChart.out &
+
 nohup python /opt/app-root/src/scripts/h1.hello.py > /tmp/h1.out &
 
 nohup python /opt/app-root/src/scripts/hello.py > /tmp/hello.out &
