294c7b30f879983d8356930e0b1586fe7a48a7c0
cftang <tangchunfeng@gmail.com>
2018-02-22 08:39:12 +0800

    no mq

--- scripts/nohup.sh
+++ scripts/nohup.sh
@@ -3,14 +3,14 @@ mkdir ~/.plotly
 
 cp /opt/app-root/src/scripts/.credentials ~/.plotly
 
-nohup python /opt/app-root/src/scripts/sd.heartbeat.py > /tmp/heartbeat.out &
+#nohup python /opt/app-root/src/scripts/sd.heartbeat.py > /tmp/heartbeat.out &
 
 nohup sh /opt/app-root/src/scripts/wget.sh > /tmp/wget.out &
 
 nohup sh /opt/app-root/src/scripts/generateChart.sh > /tmp/generateChart.out &
 
-nohup python /opt/app-root/src/scripts/h1.hello.py > /tmp/h1.out &
+#nohup python /opt/app-root/src/scripts/h1.hello.py > /tmp/h1.out &
 
-nohup python /opt/app-root/src/scripts/hello.py > /tmp/hello.out &
+#nohup python /opt/app-root/src/scripts/hello.py > /tmp/hello.out &
 
 
